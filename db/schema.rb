55175da60f0e92f2d5c83b10c4f01a1c9eda3499
João Paulo <joaopaulo.oliveira83@gmail.com>
2022-07-23 03:31:04 -0300

    Create join table client loan product, create controller client

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_062622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
   create_enum "loan_type", ["EMPRESTIMO_CONSIGNADO", "EMPRESTIMO_PESSOAL", "EMPRESTIMO_GARANTIA"]
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "cpf", null: false
+    t.integer "age"
+    t.datetime "birth_date", precision: nil, null: false
+    t.string "uf", null: false
+    t.float "incom", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clients_loan_products", id: false, force: :cascade do |t|
+    t.bigint "client_id", null: false
+    t.bigint "loan_product_id", null: false
+    t.index ["client_id", "loan_product_id"], name: "index_clients_loan_products_on_client_id_and_loan_product_id"
+    t.index ["loan_product_id", "client_id"], name: "index_clients_loan_products_on_loan_product_id_and_client_id"
+  end
+
+  create_table "loan_products", force: :cascade do |t|
+    t.string "name"
+    t.enum "loan_type", enum_type: "loan_type"
+    t.float "interest_rate", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
