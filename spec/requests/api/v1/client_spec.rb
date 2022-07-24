32a04f28fba47bd9e16d1779763b824826ccc8bd
João Paulo <joaopaulo.oliveira83@gmail.com>
2022-07-24 00:07:57 -0300

    Add tests for consignado, pessoal and garantia

--- spec/requests/api/v1/client_spec.rb
+++ spec/requests/api/v1/client_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe "Api::V1::Clients", type: :request do
     LoanProduct.create(loan_type: 'EMPRESTIMO_CONSIGNADO',  interest_rate: 2)
     client = Client.create(name: 'Joao', cpf: '00000', age: 32, birth_date: '1990/03/17', uf: 'PR', income: '3000')
 
-    post '/api/v1/clients', params: { client: { name: 'Joao', cpf: '12345678911', age: 32, birth_date: '1990/03/17', uf: 'PR', income: '3000' } }
+    post '/api/v1/clients', params: { client: { name: 'Joao', cpf: '12345678911', birth_date: '1990/03/17', uf: 'PR', income: '3000' } }
    
     expect(response).to have_http_status(201)
     expect(response.body).to eq("{\"name\":\"Joao\",\"cpf\":\"12345678911\",\"age\":32,\"uf\":\"PR\",\"renda_mensal\":3000.0,\"produto_emprestismo\":[{\"tipo_empretismo\":\"EMPRESTIMO_PESSOAL\",\"taxa_juros\":\"4.0%\"}]}")
@@ -46,7 +46,7 @@ RSpec.describe "Api::V1::Clients", type: :request do
     LoanProduct.create(loan_type: 'EMPRESTIMO_GARANTIA', interest_rate: 3)
     LoanProduct.create(loan_type: 'EMPRESTIMO_CONSIGNADO',  interest_rate: 2)
     
-    post '/api/v1/clients', params: { client: { name: 'Joao', cpf: '12345678911', age: 29, birth_date: '1990/03/17', uf: 'PR', income: '5000' } }
+    post '/api/v1/clients', params: { client: { name: 'Joao', cpf: '12345678911', birth_date: '1990/03/17', uf: 'PR', income: '5000' } }
 
     expect(response).to have_http_status(201)
     expect(response.body).to eq("{\"name\":\"Joao\",\"cpf\":\"12345678911\",\"age\":32,\"uf\":\"PR\",\"renda_mensal\":5000.0,\"produto_emprestismo\":[{\"tipo_empretismo\":\"EMPRESTIMO_PESSOAL\",\"taxa_juros\":\"4.0%\"},{\"tipo_empretismo\":\"EMPRESTIMO_CONSIGNADO\",\"taxa_juros\":\"2.0%\"}]}")
@@ -57,9 +57,21 @@ RSpec.describe "Api::V1::Clients", type: :request do
     LoanProduct.create(loan_type: 'EMPRESTIMO_GARANTIA', interest_rate: 3)
     LoanProduct.create(loan_type: 'EMPRESTIMO_CONSIGNADO',  interest_rate: 2)
     
-    post '/api/v1/clients', params: { client: { name: 'Joao', cpf: '12345678911', age: 29, birth_date: '1990/03/17', uf: 'sp', income: '3000' } }
+    post '/api/v1/clients', params: { client: { name: 'Joao', cpf: '12345678911', birth_date: '1990/03/17', uf: 'sp', income: '3000' } }
    
     expect(response).to have_http_status(201)
     expect(response.body).to eq("{\"name\":\"Joao\",\"cpf\":\"12345678911\",\"age\":32,\"uf\":\"SP\",\"renda_mensal\":3000.0,\"produto_emprestismo\":[{\"tipo_empretismo\":\"EMPRESTIMO_PESSOAL\",\"taxa_juros\":\"4.0%\"},{\"tipo_empretismo\":\"EMPRESTIMO_GARANTIA\",\"taxa_juros\":\"3.0%\"}]}")
   end
+
+  it 'should create a client with loan product EMPRESTIMO_PESSOAL, EMPRESTIMO_GARANTIA and EMPRESTIMO_CONSIGNADO' do
+    LoanProduct.create(loan_type: 'EMPRESTIMO_PESSOAL', interest_rate: 4)
+    LoanProduct.create(loan_type: 'EMPRESTIMO_GARANTIA', interest_rate: 3)
+    LoanProduct.create(loan_type: 'EMPRESTIMO_CONSIGNADO',  interest_rate: 2)
+    
+    post '/api/v1/clients', params: { client: { name: 'Joao', cpf: '12345678911', birth_date: '1993/03/17', uf: 'PR', income: '5000' } }
+    
+    expect(response).to have_http_status(201)
+    expect(response.body).to eq("{\"name\":\"Joao\",\"cpf\":\"12345678911\",\"age\":29,\"uf\":\"PR\",\"renda_mensal\":5000.0,\"produto_emprestismo\":[{\"tipo_empretismo\":\"EMPRESTIMO_PESSOAL\",\"taxa_juros\":\"4.0%\"},{\"tipo_empretismo\":\"EMPRESTIMO_GARANTIA\",\"taxa_juros\":\"3.0%\"},{\"tipo_empretismo\":\"EMPRESTIMO_CONSIGNADO\",\"taxa_juros\":\"2.0%\"}]}")
+  end
+
 end
